077b881a6581da415c66a409484b197c91807ce1
Jordan Bachelez <bachelezjordan@gmail.com>
2022-07-19 16:03:26 +0200

    base backend

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const helmet = require("helmet");
 require("dotenv").config();
 
 mongoose
@@ -29,6 +30,20 @@ app.use((req, res, next) => {
 
 app.use((req, res, next) => {
   console.log("Réponse envoyée avec succès !");
+});
+
+app.use(helmet());
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
 });
 
 app.use(express.json());
