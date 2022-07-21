38bed986a4223038ed3c0011f13d295eb62f343d
Jordan Bachelez <bachelezjordan@gmail.com>
2022-07-21 16:35:44 +0200

    password user

---
+++ backend/models/password.js
@@ -0,0 +1,23 @@
+const passwordValidator = require("password-validator");
+
+const passwordSchema = new passwordValidator();
+
+passwordSchema
+  .is()
+  .min(10)
+  .is()
+  .max(64)
+  .has()
+  .uppercase()
+  .has()
+  .lowercase()
+  .has()
+  .digits()
+  .has()
+  .not()
+  .spaces()
+  .is()
+  .not()
+  .oneOf(["Passw0rd", "Password123"]);
+
+module.exports = passwordSchema;
