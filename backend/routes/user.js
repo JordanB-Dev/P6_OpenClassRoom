76fabf459d0a4ede2925b034825f1ee431cd596d
Jordan Bachelez <bachelezjordan@gmail.com>
2022-07-28 16:11:52 +0200

    regex email

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const router = express.Router();
 
+const raterLimit = require("express-rate-limit");
+
+const limiter = raterLimit({
+  windowMs: 5 * 60 * 1000,
+  max: 50,
+});
+
 const userCtrl = require("../controllers/user");
 const checkPassword = require("../middleware/checkPassword");
+const checkEmail = require("../middleware/checkEmail");
 
-router.post("/signup", checkPassword, userCtrl.signup);
-router.post("/login", userCtrl.login);
+router.post("/signup", checkEmail, checkPassword, userCtrl.signup);
+router.post("/login", limiter, userCtrl.login);
 
 module.exports = router;
