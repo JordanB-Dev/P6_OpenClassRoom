9f12ff05d354ebca69f4856b73f9da6225d5cce6
Jordan Bachelez <bachelezjordan@gmail.com>
2022-08-25 03:06:38 +0200

    fix

--- backend/middleware/checkPassword.js
+++ backend/middleware/checkPassword.js
@@ -3,7 +3,7 @@ module.exports = (req, res, next) => {
   if (!passwordSchema.validate(req.body.password)) {
     res.writeHead(
       400,
-      '{"message":"Mot de passe requis : 10 caractères minimun. Au moins 1 Majuscule, 1 minuscule."}',
+      "Mot de passe requis : 10 caractères minimun. Au moins 1 Majuscule, 1 minuscule.",
       {
         "content-type": "application/json",
       }
